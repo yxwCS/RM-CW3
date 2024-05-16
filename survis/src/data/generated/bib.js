9392a28b77f1a09ed5a74bc2970a4ecbccf584e6
yxwCS <168489173+yxwCS@users.noreply.github.com>
2024-05-16 01:01:54 +0100

    Add files via upload

---
+++ survis/src/data/generated/bib.js
@@ -0,0 +1,93 @@
+﻿define({ entries : {
+    "10.1007/978-3-030-35055-0_7": {
+        "abstract": "\"Internet users increasingly rely on commercial virtual private network (VPN) services to protect their security and privacy. The VPN services route the client's traffic over an encrypted tunnel to a VPN gateway in the cloud. Thus, they hide the client's real IP address from online services, and they also shield the user's connections from perceived threats in the access networks. In this paper, we study the security of such commercial VPN services. The focus is on how the client applications set up VPN tunnels, and how the service providers instruct users to configure generic client software. We analyze common VPN protocols and implementations on Windows, macOS and Ubuntu. We find that the VPN clients have various configuration flaws, which an attacker can exploit to strip off traffic encryption or to bypass authentication of the VPN gateway. In some cases, the attacker can also steal the VPN user's username and password. We suggest ways to mitigate each of the discovered vulnerabilities.\",",
+        "address": "\"Cham\",",
+        "author": "\"Bui, Thanh and Rao, Siddharth and Antikainen, Markku and Aura, Tuomas\",",
+        "booktitle": "\"Secure IT Systems\",",
+        "editor": "\"Askarov, Aslan and Hansen, Ren{\\'e} Rydhof and Rafnsson, Willard\",",
+        "isbn": "\"978-3-030-35055-0\"",
+        "pages": "\"103--119\",",
+        "publisher": "\"Springer International Publishing\",",
+        "title": "\"Client-Side Vulnerabilities in Commercial VPNs\",",
+        "type": "InProceedings",
+        "year": "\"2019\","
+    },
+    "abbas2023security": {
+        "author": "Abbas, Haider and Emmanuel, Naina and Amjad, Muhammad Faisal and Yaqoob, Tahreem and Atiquzzaman, Mohammed and Iqbal, Zafar and Shafqat, Narmeen and Shahid, Waleed Bin and Tanveer, Ali and Ashfaq, Umer",
+        "journal": "ACM Computing Surveys",
+        "number": "13s",
+        "pages": "1--47",
+        "publisher": "ACM New York, NY",
+        "title": "Security Assessment and Evaluation of VPNs: A Comprehensive Survey",
+        "type": "article",
+        "volume": "55",
+        "year": "2023"
+    },
+    "abdulazeez2020comparison": {
+        "author": "Abdulazeez, Adnan and Salim, Baraa and Zeebaree, Diyar and Doghramachi, Dana",
+        "publisher": "International Association of Online Engineering",
+        "title": "Comparison of VPN Protocols at Network Layer Focusing on Wire Guard Protocol",
+        "type": "article",
+        "year": "2020"
+    },
+    "ajiya2019performance": {
+        "author": "Ajiya, AA and Idriss, US and Jerome, MG",
+        "journal": "International Journal of Computer Applications",
+        "pages": "8887",
+        "title": "Performance evaluation of IPSEC-VPN on debian linux environment",
+        "type": "article",
+        "volume": "975",
+        "year": "2019"
+    },
+    "jahan2017application": {
+        "author": "Jahan, Sohely and Rahman, Md Saifur and Saha, Sajeeb",
+        "booktitle": "2017 international conference on networking, systems and security (nsyss)",
+        "organization": "IEEE",
+        "pages": "39--44",
+        "title": "Application specific tunneling protocol selection for Virtual Private Networks",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "khan2018empirical": {
+        "author": "Khan, Mohammad Taha and DeBlasio, Joe and Voelker, Geoffrey M and Snoeren, Alex C and Kanich, Chris and Vallina-Rodriguez, Narseo",
+        "booktitle": "Proceedings of the Internet Measurement Conference 2018",
+        "pages": "443--456",
+        "title": "An empirical analysis of the commercial vpn ecosystem",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "lackorzynski2019comparative": {
+        "author": "Lackorzynski, Tim and K{\\\"o}psell, Stefan and Strufe, Thorsten",
+        "booktitle": "2019 15th IEEE International Workshop on Factory Communication Systems (WFCS)",
+        "organization": "IEEE",
+        "pages": "1--8",
+        "title": "A comparative study on virtual private networks for future industrial communication systems",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "ramesh2022vpnalyzer": {
+        "author": "Ramesh, Reethika and Evdokimov, Leonid and Xue, Diwen and Ensafi, Roya",
+        "booktitle": "Network and Distributed System Security",
+        "title": "VPNalyzer: systematic investigation of the VPN ecosystem",
+        "type": "inproceedings",
+        "volume": "10",
+        "year": "2022"
+    },
+    "wilson2020investigation": {
+        "author": "Wilson, Jack and McLuskie, David and Bayne, Ethan",
+        "booktitle": "Proceedings of the 15th International Conference on Availability, Reliability and Security",
+        "pages": "1--9",
+        "title": "Investigation into the security and privacy of iOS VPN applications",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "wu2019performance": {
+        "author": "Wu, Zheng and Xiao, Mingzhong",
+        "booktitle": "2019 IEEE 2nd International Conference on Electronics Technology (ICET)",
+        "organization": "IEEE",
+        "pages": "51--55",
+        "title": "Performance evaluation of VPN with different network topologies",
+        "type": "inproceedings",
+        "year": "2019"
+    }
+}});
