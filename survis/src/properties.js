3ef644313649f97ae07e7aaba0b2248085249b36
yxwCS <168489173+yxwCS@users.noreply.github.com>
2024-05-17 09:59:54 +0100

    Update properties.js

--- survis/src/properties.js
+++ survis/src/properties.js
@@ -2,7 +2,7 @@
  * Name as used as title for the main page
  * @type {string}
  */
-var title = 'SurVis Literature Collection';
+var title = 'VPN Security Survey';
 
 /**
  * Relative path to the data directory
@@ -54,7 +54,7 @@ var editable = true;
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
+    html: 'Literature Review on Commercial VPN Security <b><a href="https://yxwcs.github.io/RM-CW3/" target="_blank">SurVis</a></b>',
     id: 'Beck2016Visual'
 };
 
